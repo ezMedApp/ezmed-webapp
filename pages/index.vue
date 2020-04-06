fa7000f1157d1a7e1f240f3bc153ab117a0e6bb6
Daniel Romero <romerodaniel.k90@gmail.com>
2020-04-05 22:50:09 -0700

    save images; set global styling; home hero section WIP;

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,34 @@
 <script lang="ts">
-import Vue from "vue";
-import Logo from "~/components/Logo.vue";
+// import Vue from "vue";
+// import Logo from "~/components/Logo.vue";
 
-export default Vue.extend({
-  components: {
-    Logo
-  }
-});
+// export default Vue.extend({
+//   components: {
+//     Logo
+//   }
+// });
 </script>
 
 <template>
-  <div class="container">
+  <div class="hero">
     <div>
-      <logo />
       <h1 class="title">
-        ezmed
+        ezMed
       </h1>
       <h2 class="subtitle">
-        My praiseworthy Nuxt.js project
+        Eum velit, molestiae modi debitis iusto?
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
     </div>
   </div>
 </template>
 
-<style>
-.container {
+<style lang="scss">
+.hero {
+  background-image: url("/images/homepage/athlete-boardwalk-small.jpg");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
   margin: 0 auto;
-  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -46,24 +36,39 @@ export default Vue.extend({
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: var(--theme-font-heading);
   display: block;
   font-weight: 300;
   font-size: 100px;
-  color: #35495e;
+  color: var(--theme-white);
   letter-spacing: 1px;
 }
 
 .subtitle {
   font-weight: 300;
   font-size: 42px;
-  color: #526488;
+  color: var(--theme-white-alt);
   word-spacing: 5px;
   padding-bottom: 15px;
 }
 
-.links {
-  padding-top: 15px;
+@media (orientation: portrait) and (max-width: 40em) {
+  .hero {
+    height: 100vh;
+    max-height: 30em;
+  }
+}
+
+@media (min-width: 40em) {
+  .hero {
+    min-height: 100vh;
+    background-image: url("/images/homepage/athlete-boardwalk.jpg");
+  }
+}
+
+@media (min-width: 60em) {
+  .hero {
+    background-image: url("/images/homepage/athlete-boardwalk-large.jpg");
+  }
 }
 </style>
