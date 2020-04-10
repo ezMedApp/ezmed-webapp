2694828725688d20de621d7d54fcf4de9beae1d2
Daniel Romero <romerodaniel.k90@gmail.com>
2020-04-09 23:34:05 -0700

    set homepage lorem ipsum; basic layout for home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,112 @@
 <script lang="ts">
-// import Vue from "vue";
-// import Logo from "~/components/Logo.vue";
-
-// export default Vue.extend({
-//   components: {
-//     Logo
-//   }
-// });
 </script>
 
 <template>
   <div>
     <div class="hero">
-      <div class="container">
-        <h1 class="title">
-          ezMed
-        </h1>
-        <h2 class="subtitle">
-          Eum velit, molestiae modi debitis iusto?
-        </h2>
+      <div class="container-block">
+        <div class="container">
+          <h1 class="title">
+            ezMed
+          </h1>
+          <h2 class="subtitle">
+            Eum velit, molestiae modi debitis iusto?
+          </h2>
+        </div>
       </div>
     </div>
 
     <div class="main">
-      <div class="container">
-        <h3>Keep Track of</h3>
-        <ul>
-          <li>Symptoms</li>
-          <li>Medication</li>
-          <li>Exercise</li>
-        </ul>
+      <div class="container-block">
+        <div class="container section">
+          <h3 class="text-center">
+            On Track
+          </h3>
+          <ul class="list row">
+            <li class="list-item-theme-decoration">
+              Symptoms
+            </li>
+            <li class="list-item-theme-decoration">
+              Medication
+            </li>
+            <li class="list-item-theme-decoration">
+              Exercise
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="main dark-background">
+      <div class="container-block">
+        <div class="row-tablet">
+          <!-- use srcset and other queries to get the other sizes  -->
+          <div class="container section">
+            <img
+              src="/images/homepage/stethoscope.jpg"
+              alt="Image of a stethoscope."
+            >
+          </div>
+
+          <div class="container section symptoms-content">
+            <h3>Share your symptoms</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe dolor est,
+              officiis quasi numquam ipsam necessitatibus quas magni ex perspiciatis,
+              ab eius similique quisquam suscipit error, quo voluptate asperiores! Aperiam?
+            </p>
+          </div>
+        </div>
       </div>
     </div>
 
     <div class="main">
-      <div class="container">
-        <!-- use srcset and other queries to get the other sizes  -->
-        <img
-          src="/images/homepage/stethoscope.jpg"
-          alt="Image of a stethoscope."
-        >
-        <h3>Share your symptoms</h3>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe dolor est,
-          officiis quasi numquam ipsam necessitatibus quas magni ex perspiciatis,
-          ab eius similique quisquam suscipit error, quo voluptate asperiores! Aperiam?
-        </p>
+      <div class="container-block">
+        <div class="row-tablet reverse-row">
+          <div class="container section">
+            <img
+              src="/images/homepage/woman-wearing-mask.jpg"
+              alt="Image of a stethoscope."
+            >
+          </div>
+
+          <div class="container section">
+            <h3>Trace Symptoms</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe dolor est,
+              officiis quasi numquam ipsam necessitatibus quas magni ex perspiciatis,
+              ab eius similique quisquam suscipit error, quo voluptate asperiores! Aperiam?
+            </p>
+          </div>
+        </div>
       </div>
     </div>
-    
-    <div class="main">
-      <div class="container">
-        <img
-          src="/images/homepage/woman-wearing-mask.jpg"
-          alt="Image of a stethoscope."
-        >
-        <h3>Protect yourself</h3>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe dolor est,
-          officiis quasi numquam ipsam necessitatibus quas magni ex perspiciatis,
-          ab eius similique quisquam suscipit error, quo voluptate asperiores! Aperiam?
-        </p>
+
+    <div class="main dark-background">
+      <div class="container-block">
+        <div class="row-tablet">
+          <div class="container">
+            <h4>Find Us At</h4>
+            <ul class="list">
+              <li>gram</li>
+              <li>twitter</li>
+              <li>fb</li>
+            </ul>
+          </div>
+
+          <div class="container" />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
+.dark-background {
+  background-color: var(--theme-tertiary-color);
+  color: var(--theme-main-color);
+}
+
 .hero {
   background-image: url("/images/homepage/athlete-boardwalk-small.jpg");
   background-repeat: no-repeat;
@@ -79,6 +119,19 @@
   text-align: center;
 }
 
+.section {
+  padding-top: 2.5em;
+  padding-bottom: 5em;
+}
+
+.symptoms-content {
+  margin-top: -5em;
+}
+
+.reverse-row {
+  flex-direction: row-reverse;
+}
+
 @media (orientation: portrait) and (max-width: 40em) {
   .hero {
     height: 100vh;
@@ -87,9 +140,25 @@
 }
 
 @media (min-width: 40em) {
+  .section {
+    padding-top: 5em;
+  }
+
   .hero {
     min-height: 100vh;
     background-image: url("/images/homepage/athlete-boardwalk.jpg");
+  }
+
+  .row-tablet {
+    display: flex;
+
+    .container {
+      flex: 1;
+    }
+  }
+
+  .symptoms-content {
+    margin-top: 0;
   }
 }
 
